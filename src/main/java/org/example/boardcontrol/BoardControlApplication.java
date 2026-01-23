e649f465d58a11efabc3bef215b88e5d20235a77
Andre Lucas <126403873+Andrelcs12@users.noreply.github.com>
2026-01-23 11:44:16 -0300

    feat(core): Project and Task entities with relationships

---
+++ src/main/java/org/example/boardcontrol/BoardControlApplication.java
@@ -0,0 +1,13 @@
+package org.example.boardcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/boardcontrol/BoardControlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boardcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
